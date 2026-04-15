9c27502f96d4abb4608c2081f70192e1dfdc9e23
marcelopenas <marcelopenas@outlook.com>
2026-04-15 13:20:05 -0300

    format main

--- main.py
+++ main.py
@@ -1,65 +1,61 @@
-from flask import Flask, request, jsonify
-from db import db
-from models import User
 import os
+
+from flask import Flask, Response, jsonify, request
 from flask_cors import CORS
 
-def create_app():
+from db import db
+from models import User
+
+
+def create_app() -> Flask:
     app = Flask(__name__)
     CORS(app)
-    
-    postgres_user = os.environ.get('POSTGRES_USER', 'appuser')
-    postgres_password = os.environ.get('POSTGRES_PASSWORD', 'apppass')
-    postgres_url = os.environ.get('POSTGRES_URL', 'localhost')
-    
+
+    postgres_user = os.environ.get("POSTGRES_USER", "appuser")
+    postgres_password = os.environ.get("POSTGRES_PASSWORD", "apppass")
+    postgres_url = os.environ.get("POSTGRES_URL", "localhost")
+
     # Define o padrão, mas permite que o Pytest sobrescreva depois
     db_uri = f"postgresql://{postgres_user}:{postgres_password}@{postgres_url}:5432/users"
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI", db_uri)
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+        "SQLALCHEMY_DATABASE_URI",
+        db_uri,
+    )
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    
+
     db.init_app(app)
 
-
     @app.route("/users", methods=["POST"])
-    def create_user():
+    def create_user() -> tuple[Response, int]:
         data = request.json
 
-        user = User(
-            name=data["name"],
-            email=data["email"]
-        )
+        user = User(name=data["name"], email=data["email"])
 
         db.session.add(user)
         db.session.commit()
 
-        return jsonify({
-            "id": str(user.id),
-            "name": user.name,
-            "email": user.email
-        }), 201
+        return jsonify(
+            {"id": str(user.id), "name": user.name, "email": user.email},
+        ), 201
 
     @app.route("/users/<uuid:user_id>", methods=["GET"])
-    def get_user(user_id):
+    def get_user(user_id: int) -> tuple[Response, int]:
         user = User.query.get_or_404(user_id)
 
-        return jsonify({
-            "id": str(user.id),
-            "name": user.name,
-            "email": user.email
-        }), 200
+        return jsonify(
+            {"id": str(user.id), "name": user.name, "email": user.email},
+        ), 200
 
     @app.route("/users/<string:email>/email", methods=["GET"])
-    def get_user_by_email(email):
+    def get_user_by_email(email: str) -> tuple[Response, int]:
         user = User.query.filter_by(email=email).first_or_404()
 
-        return jsonify({
-            "id": str(user.id),
-            "name": user.name,
-            "email": user.email
-        }), 200
+        return jsonify(
+            {"id": str(user.id), "name": user.name, "email": user.email},
+        ), 200
 
     @app.route("/users/<uuid:user_id>", methods=["DELETE"])
-    def delete_user(user_id):
+    def delete_user(user_id: int) -> tuple[str, int]:
         user = User.query.get_or_404(user_id)
 
         db.session.delete(user)
@@ -68,19 +64,16 @@ def create_app():
         return "", 204
 
     @app.route("/users", methods=["GET"])
-    def list_users():
+    def list_users() -> tuple[list, int]:
         users = User.query.all()
 
         return [
-            {
-                "id": str(user.id),
-                "name": user.name,
-                "email": user.email
-            }
+            {"id": str(user.id), "name": user.name, "email": user.email}
             for user in users
         ], 200
 
     return app
+
 
 app = create_app()
 
